3e74d901d86ddf4f605187c4eb6b97d0a15af7fa
Jose Garcia <jlg3368@gmail.com>
2015-08-23 20:05:44 -0400

    changes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,16 @@
 class UserController < ApplicationController
+  def create
+    # Create the user from params
+    @user = User.new(params[:user])
+    if @user.save
+      # Deliver the signup email
+      UserNotifier.send_signup_email(@user).deliver
+      redirect_to(@user, :notice => 'User created')
+    else
+      render :action => 'new'
+    end
+  end
+end
 
   def new
   end
@@ -9,5 +21,3 @@ class UserController < ApplicationController
 
   def about
   end
-
-end
--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,13 @@
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'Nomakk6',
+  :password => 'sebastian1',
+  :domain => 'groupboard.co',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
